ee505cf3de6eb80542ebdb16420d100445c36782
AliSaidSaritemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2021-12-12 15:27:46 +0300

    Add project files.

---
+++ RestoranSİparis/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace RestoranSİparis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnAra = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtBahsis = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMenuNo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSiparisNo = new System.Windows.Forms.TextBox();
+            this.TxtMusteriNo = new System.Windows.Forms.TextBox();
+            this.btnmenulistele = new System.Windows.Forms.Button();
+            this.btniade = new System.Windows.Forms.Button();
+            this.CbIndirim = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Highlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 414);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnListele.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.BtnListele.Location = new System.Drawing.Point(1109, 85);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(132, 37);
+            this.BtnListele.TabIndex = 53;
+            this.BtnListele.Text = "LİSTELE";
+            this.BtnListele.UseVisualStyleBackColor = false;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // BtnAra
+            // 
+            this.BtnAra.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnAra.Location = new System.Drawing.Point(1109, 336);
+            this.BtnAra.Name = "BtnAra";
+            this.BtnAra.Size = new System.Drawing.Size(132, 37);
+            this.BtnAra.TabIndex = 52;
+            this.BtnAra.Text = "ARA";
+            this.BtnAra.UseVisualStyleBackColor = false;
+            this.BtnAra.Click += new System.EventHandler(this.BtnAra_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnSil.Location = new System.Drawing.Point(1109, 219);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(132, 37);
+            this.BtnSil.TabIndex = 51;
+            this.BtnSil.Text = "SİL";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnGuncelle.Location = new System.Drawing.Point(1109, 273);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(132, 37);
+            this.BtnGuncelle.TabIndex = 50;
+            this.BtnGuncelle.Text = "GÜNCELLE";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnEkle.Location = new System.Drawing.Point(1109, 176);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(132, 37);
+            this.BtnEkle.TabIndex = 49;
+            this.BtnEkle.Text = "EKLE";
+            this.BtnEkle.UseVisualStyleBackColor = false;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Gold;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(785, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 26);
+            this.label7.TabIndex = 45;
+            this.label7.Text = "Bahşiş";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TxtBahsis
+            // 
+            this.TxtBahsis.BackColor = System.Drawing.Color.YellowGreen;
+            this.TxtBahsis.Location = new System.Drawing.Point(914, 252);
+            this.TxtBahsis.Name = "TxtBahsis";
+            this.TxtBahsis.Size = new System.Drawing.Size(132, 22);
+            this.TxtBahsis.TabIndex = 44;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gold;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(764, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 26);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Müşteri No";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(785, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 26);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Menü No";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TxtMenuNo
+            // 
+            this.TxtMenuNo.BackColor = System.Drawing.Color.YellowGreen;
+            this.TxtMenuNo.Location = new System.Drawing.Point(914, 141);
+            this.TxtMenuNo.Name = "TxtMenuNo";
+            this.TxtMenuNo.Size = new System.Drawing.Size(132, 22);
+            this.TxtMenuNo.TabIndex = 37;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(785, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 26);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "Sipariş No";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TxtSiparisNo
+            // 
+            this.TxtSiparisNo.BackColor = System.Drawing.Color.YellowGreen;
+            this.TxtSiparisNo.Location = new System.Drawing.Point(914, 100);
+            this.TxtSiparisNo.Name = "TxtSiparisNo";
+            this.TxtSiparisNo.Size = new System.Drawing.Size(132, 22);
+            this.TxtSiparisNo.TabIndex = 35;
+            // 
+            // TxtMusteriNo
+            // 
+            this.TxtMusteriNo.BackColor = System.Drawing.Color.YellowGreen;
+            this.TxtMusteriNo.Location = new System.Drawing.Point(914, 195);
+            this.TxtMusteriNo.Name = "TxtMusteriNo";
+            this.TxtMusteriNo.Size = new System.Drawing.Size(132, 22);
+            this.TxtMusteriNo.TabIndex = 54;
+            this.TxtMusteriNo.TextChanged += new System.EventHandler(this.TxtMusteriNo_TextChanged);
+            // 
+            // btnmenulistele
+            // 
+            this.btnmenulistele.BackColor = System.Drawing.SystemColors.Info;
+            this.btnmenulistele.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btnmenulistele.Location = new System.Drawing.Point(1109, 133);
+            this.btnmenulistele.Name = "btnmenulistele";
+            this.btnmenulistele.Size = new System.Drawing.Size(132, 37);
+            this.btnmenulistele.TabIndex = 55;
+            this.btnmenulistele.Text = "Menü LİSTELE";
+            this.btnmenulistele.UseVisualStyleBackColor = false;
+            this.btnmenulistele.Click += new System.EventHandler(this.btnmenulistele_Click);
+            // 
+            // btniade
+            // 
+            this.btniade.BackColor = System.Drawing.SystemColors.Info;
+            this.btniade.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.btniade.Location = new System.Drawing.Point(1109, 389);
+            this.btniade.Name = "btniade";
+            this.btniade.Size = new System.Drawing.Size(132, 37);
+            this.btniade.TabIndex = 56;
+            this.btniade.Text = "İADE ET";
+            this.btniade.UseVisualStyleBackColor = false;
+            this.btniade.Click += new System.EventHandler(this.btniade_Click);
+            // 
+            // CbIndirim
+            // 
+            this.CbIndirim.AutoSize = true;
+            this.CbIndirim.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.CbIndirim.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.CbIndirim.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.CbIndirim.ForeColor = System.Drawing.Color.Red;
+            this.CbIndirim.Location = new System.Drawing.Point(1096, 456);
+            this.CbIndirim.Name = "CbIndirim";
+            this.CbIndirim.Size = new System.Drawing.Size(195, 33);
+            this.CbIndirim.TabIndex = 57;
+            this.CbIndirim.Text = "İndirim Kullan";
+            this.CbIndirim.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1317, 583);
+            this.Controls.Add(this.CbIndirim);
+            this.Controls.Add(this.btniade);
+            this.Controls.Add(this.btnmenulistele);
+            this.Controls.Add(this.TxtMusteriNo);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.BtnAra);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.TxtBahsis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtMenuNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtSiparisNo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnAra;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtBahsis;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMenuNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSiparisNo;
+        private System.Windows.Forms.TextBox TxtMusteriNo;
+        private System.Windows.Forms.Button btnmenulistele;
+        private System.Windows.Forms.Button btniade;
+        private System.Windows.Forms.CheckBox CbIndirim;
+    }
+}
+
---
+++ RestoranSİparis/Form1.cs
@@ -0,0 +1,116 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RestoranSİparis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NpgsqlConnection baglanti = new NpgsqlConnection("server =localHost; port =5432; Database =Restorant; user ID = postgres ; Password =12345");
+        private void Cuzdan(int mmusterino)
+        {
+
+            string sorgu = "select cuzdan from musteri where musterino =";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+
+
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from siparis order by siparisno";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut1 = new NpgsqlCommand("insert into siparis (siparisno,menuno,musterino,verilistarihi,bahsis) " +
+                "values ((select otosiparisno()),@p2,@p3,@p4,@p5)", baglanti);
+            komut1.Parameters.AddWithValue("@p2", int.Parse(TxtMenuNo.Text));
+            komut1.Parameters.AddWithValue("@p3", int.Parse(TxtMusteriNo.Text));
+            komut1.Parameters.AddWithValue("@p4", DateTime.Now);
+            komut1.Parameters.AddWithValue("@p5", int.Parse(TxtBahsis.Text));
+            komut1.ExecuteNonQuery();
+            if (CbIndirim.Checked)
+            {
+             NpgsqlCommand indirim = new NpgsqlCommand("call kuponkullan(otosiparisno()-1)", baglanti);
+             indirim.ExecuteNonQuery();
+            }
+       
+            baglanti.Close();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("Delete from siparis where siparisno=@p1", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(TxtSiparisNo.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut3 = new NpgsqlCommand("update siparis set menuno=@p1,bahsis=@p2 where siparisno=@p3 ", baglanti);
+            komut3.Parameters.AddWithValue("@p3", int.Parse(TxtSiparisNo.Text));
+            komut3.Parameters.AddWithValue("@p1", int.Parse(TxtMenuNo.Text));
+            komut3.Parameters.AddWithValue("@p2", int.Parse(TxtBahsis.Text));
+            komut3.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void BtnAra_Click(object sender, EventArgs e)
+        {
+
+            string sorgu = "select * from siparis where siparisno = "+ int.Parse(TxtSiparisNo.Text);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtMusteriNo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmenulistele_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from menu";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void btniade_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            NpgsqlCommand komut2 = new NpgsqlCommand("call iade(@p1)", baglanti);
+            komut2.Parameters.AddWithValue("@p1", int.Parse(TxtSiparisNo.Text));
+            komut2.ExecuteNonQuery();
+            baglanti.Close();
+        }
+    }
+}
